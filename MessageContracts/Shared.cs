dedf7aae3da2d2db9627d77a897445af3d4fefd3
Diko <dimitarboykovboychev@gmail.com>
2025-11-17 23:58:13 +0200

    Add project files.

---
+++ MessageContracts/Shared.cs
@@ -0,0 +1,33 @@
+﻿namespace MessageContracts
+{
+    public record ProductCreated(Guid StoreID, Product Product);
+
+    public record ProductDeleted(Guid StoreID, Guid ProductId);
+
+
+    public record CreateProduct(ProductExtended ProductExtended);
+
+    public record DeleteProduct(Guid StoreID, Guid ProductId);
+
+    public class Product
+    {
+        public Guid ProductId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal MinPrice { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime UpdatedOn { get; set; }
+    }
+
+    public class ProductExtended: Product
+    {
+        public Guid StoreId { get; set; }
+    }
+}
